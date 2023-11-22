bfc3e42efc600941714f80511547b8c3ce33fe1b
eduardoramos98 <71419939+eduardoramos98@users.noreply.github.com>
2023-11-22 15:15:25 -0800

    first commit

---
+++ ejercicio1_aprende_python.py
@@ -0,0 +1,33 @@
+#Ejerciio 1 del libro Aprende Python
+#Utilizando la consola interactiva de Python >>>, realiza las siguientes tareas:
+# 1. Asigna un valor entero 2001 a la variable space_odyssey y muestra su valor. OK
+# 2. Descubre el tipo del literal 	Good night & Good luck	OK
+# 3. Identifica el tipo del literal True.
+# 4. Asigna la expresión 10 * 3.0 a la variable result y muestra su tipo.
+
+# space_odyssey = 2001
+# print(space_odyssey)
+
+###############################################################################
+
+# hola_mundo = "Good night & good luck"
+# print(type(hola_mundo))
+
+##############################################################################
+
+# active = True
+# print(active)
+
+###############################################################################
+
+# result = 10 * 3.0
+# print(type(result))
+
+###############################################################################
+#MUTABILIDAD
+a = 5
+
+b = a
+
+print(id(a))
+print(id(b))
